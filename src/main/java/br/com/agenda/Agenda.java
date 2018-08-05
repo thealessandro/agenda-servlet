e4c6f45ccd41f344d6dd21de5ba2c06706cc8ff3
Alessandro Menezes <alessandromenezes22@gmail.com>
2018-08-05 12:37:57 -0300

    add files

---
+++ src/main/java/br/com/agenda/Agenda.java
@@ -0,0 +1,28 @@
+package br.com.agenda;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/agenda")
+public class Agenda extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html></body>");
+        out.println("<h2>Agenda</h2>");
+        out.println("<br/>");
+        out.println("<a href=\"/index.html\">Voltar</a>");
+        out.println("</body></html>");
+        out.flush();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
