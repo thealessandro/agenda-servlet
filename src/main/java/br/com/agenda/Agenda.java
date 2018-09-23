36a8ccf46029586c7fee290dc1dd9a5034f62108
Alessandro Menezes <alessandromenezes22@gmail.com>
2018-09-23 12:54:06 -0300

    add log4j

--- src/main/java/br/com/agenda/Agenda.java
+++ src/main/java/br/com/agenda/Agenda.java
@@ -1,6 +1,11 @@
 package br.com.agenda;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +15,26 @@ import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/agenda")
 public class Agenda extends HttpServlet {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public Agenda() {
+        logger.info("Construindo a servlet: {}", this);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        logger.info("Inicializando a servlet: {}", this);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+        logger.info("Protocolo: {}", req.getProtocol());
+        logger.info("HTTP Method: {}", ((HttpServletRequest) req).getMethod());
+        logger.info("Request URI: {}", ((HttpServletRequest) req).getRequestURI());
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
@@ -24,5 +49,16 @@ public class Agenda extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        logger.info("Destruindo a servlet: {} ", this);
+    }
+
+    @Override
+    public String toString() {
+        return "Agenda";
     }
 }
